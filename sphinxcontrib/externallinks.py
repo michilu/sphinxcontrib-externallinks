1e97a117c2c31f7820a5e1a752ea11659295788a
MiCHiLU <djmchl@gmail.com>
2013-07-19 23:08:48 +0900

    Refactor

--- sphinxcontrib/externallinks.py
+++ sphinxcontrib/externallinks.py
@@ -1,9 +1,8 @@
 from docutils import nodes, utils
+from functools import wraps
 import urllib
 
 from sphinx.util.nodes import split_explicit_title
-
-URL = u"https://www.google.com/search?q={query}"
 
 def split_title(text):
     _has_explicit, title, other = split_explicit_title(utils.unescape(text))
@@ -12,15 +11,18 @@ def split_title(text):
 def quote_plus(text):
     return urllib.quote_plus(text.encode("utf-8")).decode("utf-8")
 
-def google_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
-    """
-    Role to Google search.
-    """
-    title, other = split_title(text)
-    query = quote_plus(other)
-    url = URL.format(query=query)
-    node = nodes.raw("", u"<a href='{url}'>{title}</a>".format(title=title, url=url), format="html")
-    return [node], []
+def gen_role(func):
+
+  @wraps(func)
+  def role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+      title, other = split_title(text)
+      node = nodes.raw("", u"<a href='{url}'>{title}</a>".format(title=title, url=func(other)), format="html")
+      return [node], []
+
+  return role
+
+def google_role(other):
+    return u"https://www.google.com/search?q={query}".format(query=quote_plus(other))
 
 def setup(app):
-    app.add_role("google", google_role)
+    app.add_role("google", gen_role(google_role))
