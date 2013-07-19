57c515f3351bf03484db6d3db714f70b3652c07a
MiCHiLU <djmchl@gmail.com>
2013-07-19 22:28:48 +0900

    Refactor

--- sphinxcontrib/externallinks.py
+++ sphinxcontrib/externallinks.py
@@ -3,14 +3,23 @@ import urllib
 
 from sphinx.util.nodes import split_explicit_title
 
+URL = u"https://www.google.com/search?q={query}"
+
+def split_title(text):
+    _has_explicit, title, other = split_explicit_title(utils.unescape(text))
+    return title, other
+
+def quote_plus(text):
+    return urllib.quote_plus(text.encode("utf-8")).decode("utf-8")
+
 def google_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     """
     Role to Google search.
     """
-    _has_explicit, title, keywords = split_explicit_title(utils.unescape(text))
-    query = urllib.quote_plus(keywords.encode("utf-8")).decode("utf-8")
-    base = u"<a href='https://www.google.com/search?q={query}'>{title}</a>"
-    node = nodes.raw("", base.format(title=title, query=query), format="html")
+    title, other = split_title(text)
+    query = quote_plus(other)
+    url = URL.format(query=query)
+    node = nodes.raw("", u"<a href='{url}'>{title}</a>".format(title=title, url=url), format="html")
     return [node], []
 
 def setup(app):
